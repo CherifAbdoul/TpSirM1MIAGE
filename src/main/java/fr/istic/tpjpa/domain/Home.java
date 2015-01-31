a9667e79bb5566a5a50bfe5b111a5a0192448afd
CherifAbdoul <awhaidara@hotmail.fr>
2015-01-31 01:53:32 +0100

    ajout des variables de la classe Home

--- src/main/java/fr/istic/tpjpa/domain/Home.java
+++ src/main/java/fr/istic/tpjpa/domain/Home.java
@@ -1,5 +1,33 @@
 package fr.istic.tpjpa.domain;
 
 public class Home {
+	
+	private long idHome;
+	private int taille;
+	private int consoMoyen;
+	
+	public Home() {
+		
+	}
+	
+	
+	public long getIdHome() {
+		return idHome;
+	}
+	public void setIdHome(long idHome) {
+		this.idHome = idHome;
+	}
+	public int getTaille() {
+		return taille;
+	}
+	public void setTaille(int taille) {
+		this.taille = taille;
+	}
+	public int getConsoMoyen() {
+		return consoMoyen;
+	}
+	public void setConsoMoyen(int consoMoyen) {
+		this.consoMoyen = consoMoyen;
+	}
 
 }
